4d48fcad25527abb4172387e05f15b7a9c8f20c1
Picani <picani@laposte.net>
2019-10-29 01:22:37 +0100

    Update executable version number

--- src/nimapod/common.nim
+++ src/nimapod/common.nim
@@ -7,7 +7,7 @@ import hashes, os, sequtils, strformat, strutils
 # The constants
 
 const appName* = "nimapod"
-const appVersion* = "2.0.0"
+const appVersion* = "2.0.2"
 const apodIndex* = "https://apod.nasa.gov/apod/archivepix.html"
 const apodApi* = "https://api.nasa.gov/planetary/apod"
 
