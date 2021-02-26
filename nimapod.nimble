b694eff483625fa0716f4a058aed50c6bd82316c
Picani <picani@laposte.net>
2021-02-26 16:05:15 +0100

    Update to version 2.0.3

--- nimapod.nimble
+++ nimapod.nimble
@@ -1,6 +1,6 @@
 # Package
 
-version       = "2.0.2"
+version       = "2.0.3"
 author        = "Picani"
 description   = "Download pictures from NASA\'s APOD"
 license       = "WTFPL"
--- src/nimapod/common.nim
+++ src/nimapod/common.nim
@@ -7,7 +7,7 @@ import hashes, os, sequtils, strformat, strutils
 # The constants
 
 const appName* = "nimapod"
-const appVersion* = "2.0.2"
+const appVersion* = "2.0.3"
 const apodIndex* = "https://apod.nasa.gov/apod/archivepixFull.html"
 const apodApi* = "https://api.nasa.gov/planetary/apod"
 
